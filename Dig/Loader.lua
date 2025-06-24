41d16c499a70dbc6ab4ef81f778222de36530ab5
Daniel <dsfxzfu@gmail.com>
2025-06-24 17:09:44 +0100

    Update Loader.lua

--- Dig/Loader.lua
+++ Dig/Loader.lua
@@ -1,10 +1,7 @@
 local GET = http.get
-
-local LOAD_STABLE_BUILD = true
 local STABLE = "https://raw.githubusercontent.com/glizzymuncher57/Scripts/refs/heads/main/Dig/Stable.lua"
-local UNSTABLE = "https://raw.githubusercontent.com/glizzymuncher57/Scripts/refs/heads/main/Dig/Unstable.lua"
 
-GET(LOAD_STABLE_BUILD and STABLE or UNSTABLE, function(Body, Status)
+GET(STABLE, function(Body, Status)
 	if Status == 200 then
 		local Chunk = load(Body)
 		if Chunk then
