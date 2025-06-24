48d926529b4b78e2fe4290d7744bdbb3a5bf78c8
Daniel <dsfxzfu@gmail.com>
2025-06-24 16:46:47 +0100

    Update loader_options.lua

--- loader_options.lua
+++ loader_options.lua
@@ -1,1 +1,19 @@
+local GET = http.get
+
+local LOAD_STABLE_BUILD = true
+local STABLE = "https://raw.githubusercontent.com/DCons0001/Scripts/refs/heads/main/DigStable.lua"
+local UNSTABLE = "https://raw.githubusercontent.com/DCons0001/Scripts/refs/heads/main/DigUnstable.lua"
+
+GET(LOAD_STABLE_BUILD and STABLE or UNSTABLE, function(Body, Status)
+    if Status == 200 then
+        local Chunk = load(Body)
+        if Chunk then
+            local Success, ErrorMessage = pcall(Chunk)
+            if not Success then
+                log.add("Error executing script: " .. tostring(ErrorMessage), color(1, 0, 0, 1))
+            end
+        end
+    end
+end)
+  
 
