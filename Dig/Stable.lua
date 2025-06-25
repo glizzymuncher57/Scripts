3a316f473293051894d606c2835ddadaee88b8cc
Daniel <dsfxzfu@gmail.com>
2025-06-25 12:08:26 +0100

    Update Stable.lua

--- Dig/Stable.lua
+++ Dig/Stable.lua
@@ -5,71 +5,71 @@ local DEBUG = true
 local DIGGING = false
 
 local CONFIG = {
-    Tolerance = 33, -- Distance tolerance for clicking.
-    WaitWhenClicked = 50, -- Wait time when clicked (miliseconds)
-    WaitWhenNotClicked = 0, -- Wait time when not clicked (miliseconds)
+	Tolerance = 27, -- Distance tolerance for clicking.
+	WaitWhenClicked = 125, -- Wait time when clicked (miliseconds)
+	WaitWhenNotClicked = 25, -- Wait time when not clicked (miliseconds)
 }
 
 local function logFunc(...)
-    if DEBUG then
-        log.add(..., color(1, 0, 0, 1))
-    end
+	if DEBUG then
+		log.add(..., color(1, 0, 0, 1))
+	end
 end
 
 local function ReturnFirstDescendant(Parent, Name)
-    for _, Descendant in pairs(Parent:get_descendants()) do
-        if Descendant.name == Name then
-            return Descendant
-        end
-    end
+	for _, Descendant in pairs(Parent:get_descendants()) do
+		if Descendant.name == Name then
+			return Descendant
+		end
+	end
 
-    return nil_instance
+	return nil_instance
 end
 
 local function StartTheDiggering()
-    -- probably heavily unoptimised to check for the ui every loop but it shouldn't make that much of a diff.
-    while DIGGING do
-        local DigUI = PlayerGui:find_first_child("Dig")
-        if not DigUI:isvalid() then
-            logFunc("Dig UI became invalid")
-            DIGGING = false
-            return
-        end
+	-- probably heavily unoptimised to check for the ui every loop but it shouldn't make that much of a diff.
+	while DIGGING do
+		local DigUI = PlayerGui:find_first_child("Dig")
+		if not DigUI:isvalid() then
+			logFunc("Dig UI became invalid")
+			DIGGING = false
+			return
+		end
 
-        local Area_Strong = ReturnFirstDescendant(DigUI, "Area_Strong")
-        if not Area_Strong:isvalid() then
-            logFunc("Area_Strong became invalid")
-            DIGGING = false
-            return
-        end
+		local Area_Strong = ReturnFirstDescendant(DigUI, "Area_Strong")
+		if not Area_Strong:isvalid() then
+			logFunc("Area_Strong became invalid")
+			DIGGING = false
+			return
+		end
 
-        local PlayerBar = ReturnFirstDescendant(DigUI, "PlayerBar")
-        if not PlayerBar:isvalid() then
-            logFunc("PlayerBar became invalid")
-            DIGGING = false
-            return
-        end
+		local PlayerBar = ReturnFirstDescendant(DigUI, "PlayerBar")
+		if not PlayerBar:isvalid() then
+			logFunc("PlayerBar became invalid")
+			DIGGING = false
+			return
+		end
 
-        local success, err = pcall(function()
-            local PlayerBarPos = PlayerBar.gui_position + (PlayerBar.gui_size / 2)
-            local Area_StrongPos = Area_Strong.gui_position + (Area_Strong.gui_size / 2)
-            local Distance = math.abs(PlayerBarPos.x - Area_StrongPos.x)
+		local success, err = pcall(function()
+			local PlayerBarPos = PlayerBar.gui_position + (PlayerBar.gui_size / 2)
+			local Area_StrongPos = Area_Strong.gui_position + (Area_Strong.gui_size / 2)
+			local Distance = math.abs(PlayerBarPos.x - Area_StrongPos.x)
 
-            local Clicked = false
-            if Distance <= CONFIG.Tolerance then
-                input.simulate_mouse_click(MOUSE1)
-                Clicked = true
-            end
+			local Clicked = false
+			if Distance <= CONFIG.Tolerance then
+				input.simulate_mouse_click(MOUSE1)
+				Clicked = true
+			end
 
-            wait(Clicked and CONFIG.WaitWhenClicked or CONFIG.WaitWhenNotClicked)
-        end)
+			wait(Clicked and CONFIG.WaitWhenClicked or CONFIG.WaitWhenNotClicked)
+		end)
 
-        if not success then
-            logFunc("Error in dig loop: " .. tostring(err))
-            DIGGING = false
-            return
-        end
-    end
+		if not success then
+			logFunc("Error in dig loop: " .. tostring(err))
+			DIGGING = false
+			return
+		end
+	end
 end
 
 local ui = gui.create("Dig Settings", false)
@@ -78,23 +78,23 @@ ui:set_size(400, 200)
 
 local slider = ui:add_slider("slider1", "Tolerance - Supports Decimals", 0, 150, CONFIG.Tolerance)
 slider:change_callback(function()
-    CONFIG.Tolerance = slider:get_value()
+	CONFIG.Tolerance = slider:get_value()
 end)
 
 local slider2 = ui:add_slider("slider2", "Wait When Clicked - Doesn't Support Decimals", 0, 200, CONFIG.WaitWhenClicked)
 slider2:change_callback(function()
-    CONFIG.WaitWhenClicked = math.floor(slider2:get_value())
+	CONFIG.WaitWhenClicked = math.floor(slider2:get_value())
 end)
 
 local slider3 = ui:add_slider("slider3", "Wait When Not Clicked - Doesn't Support Decimals", 0, 200, CONFIG.WaitWhenNotClicked)
 slider3:change_callback(function()
-    CONFIG.WaitWhenNotClicked = math.floor(slider3:get_value())
+	CONFIG.WaitWhenNotClicked = math.floor(slider3:get_value())
 end)
 
 hook.addkey(0x51, "womp", function(KD)
-    DIGGING = KD
+	DIGGING = KD
 
-    if DIGGING then
-        coroutine.resume(coroutine.create(StartTheDiggering))
-    end
+	if DIGGING then
+		coroutine.resume(coroutine.create(StartTheDiggering))
+	end
 end)
