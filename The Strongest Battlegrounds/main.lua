1c8ebf2dbb61a0513386b9c471e3fed8d1db8783
Daniel <dsfxzfu@gmail.com>
2025-06-26 15:16:45 +0100

    Update main.lua
    changed binds fixed lctrl held down logic

--- The Strongest Battlegrounds/main.lua
+++ The Strongest Battlegrounds/main.lua
@@ -2,66 +2,65 @@ local players = game:get_service("Players")
 local localplayer = players.local_player
 
 local VK_SHIFT = 0x10
-local VK_LSHIFT  = 0xA0
+local VK_LSHIFT = 0xA0
 local VK_LCTRL = 0xA0
 
 local POSITIONS = {
-	[0x31] = {
+	[0x5A] = {
 		Name = "Center of Map",
 		Position = vector3(152, 440, 34),
 	},
 
-	[0x32] = {
+	[0x58] = {
 		Name = "Ball Room",
 		Position = vector3(1064, 146, 23020),
 	},
 
-	[0x33] = {
+	[0x43] = {
 		Name = "Fist Room",
 		Position = vector3(-67, 29, 20339),
 	},
 
-	[0x34] = {
+	[0x56] = {
 		Name = "Random Hole",
 		Position = vector3(438, 439, -376),
 	},
 }
 
 local function GetPosition(Index)
-    return POSITION_TABLE[Index] or vector3(152, 440, 34)
+	return POSITION_TABLE[Index] or vector3(152, 440, 34)
 end
 
 local function TeleportToPosition(vector3position)
-    if localplayer and localplayer.character then
-        local humanoidrootpart = localplayer.character:find_first_child("HumanoidRootPart")
-        if not humanoidrootpart then
-            return
-        end
+	if localplayer and localplayer.character then
+		local humanoidrootpart = localplayer.character:find_first_child("HumanoidRootPart")
+		if not humanoidrootpart then
+			return
+		end
 
-        humanoidrootpart:set_position(vector3position)
-    end
+		humanoidrootpart:set_position(vector3position)
+	end
 end
 
 local function Init()
-    log.notification("teleport to teleport to an area.", "Info")
-    log.notification("Press Shift + 1-4 to..", "Info")
+	log.notification("teleport to teleport to an area.", "Info")
+	log.notification("Press LCTRL + Z, X, C, V.", "Info")
 
-    for Keycode, Info in pairs(POSITIONS) do
-        hook.addkey(Keycode, tostring(Keycode), function()
+	for Keycode, Info in pairs(POSITIONS) do
+		hook.addkey(Keycode, tostring(Keycode), function(keydown)
 			local IsLCtrlDown = input.key_down(VK_LCTRL)
-            local IsShiftDown = input.key_down(VK_SHIFT) or input.key_down(VK_LSHIFT)
-
-			if not IsShiftDown or not IsLCtrlDown then
+			if not IsLCtrlDown then
 				return
 			end
 
-            local IsKeyDown = input.key_down(Keycode)
-            if not IsKeyDown then return end
+			if not keydown then
+				return
+			end
 
-            TeleportToPosition(Info.Position)
-		log.notification("Teleported to " .. Info.Name, "Info")
-        end)
-    end
+			TeleportToPosition(Info.Position)
+			og.notification("Teleported to " .. Info.Name, "Info")
+		end)
+	end
 end
 
 Init()
