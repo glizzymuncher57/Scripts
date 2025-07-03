3f62120eb94f4a56265bba77549e6c488a63b78f
Daniel <dsfxzfu@gmail.com>
2025-07-03 17:54:08 +0100

    Update NPC Support [LEGACY].lua

--- Games/The Final Stand 2/NPC Support [LEGACY].lua
+++ Games/The Final Stand 2/NPC Support [LEGACY].lua
@@ -1,6 +1,7 @@
 local entities = game:get_service("Workspace"):find_first_child("Zombies")
 local hook = hook
 
+force_custom_players()
 hook.add("init_custom_entity", "asdasdasd", function()
     for _, e in pairs(entities:get_children()) do
         if e:isvalid() then
